6487934f2b208c89f07ce4f724e59071bdc4c9c2
anvi <andreasgylche@gmail.com>
2024-12-03 19:49:57 +0100

    day 3 part 1 done

--- 3/go/main.go
+++ 3/go/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,5 +17,32 @@ func main() {
 	rgx, err := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
 	instructions := rgx.FindAllString(string(b), -1)
 
-	fmt.Println(instructions)
+	rgx, err = regexp.Compile(`\d{1,3},\d{1,3}`)
+	if err != nil {
+		panic(err)
+	}
+
+	total := 0
+
+	for _, instruction := range instructions {
+		ds := strings.Split(rgx.FindString(instruction), ",")
+		if len(ds) != 2 {
+			panic("ballsack")
+		}
+
+		int1, err := strconv.Atoi(ds[0])
+		if err != nil {
+			panic(err)
+		}
+
+		int2, err := strconv.Atoi(ds[1])
+		if err != nil {
+			panic(err)
+		}
+
+		total += int1 * int2
+
+	}
+
+	fmt.Println(total)
 }
